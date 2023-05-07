ed74c13b1d759211bcdb7dd3e764c94bcf97fb72
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-05-07 10:22:48 +0530

    array1

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -16,10 +16,8 @@ public class BinarySearch {
                 end = mid - 1;
             }
         }
-return -1;
+        return -1;
     }
-
-
     public static void main(String[] args) {
 
         int arr[] = {2, 4, 6, 8, 10, 12, 14, 16};
