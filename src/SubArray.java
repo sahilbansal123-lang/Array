2eaee921525302c1ca94b94da14b0a2209a41948
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-05-08 10:06:11 +0530

    array1

--- src/SubArray.java
+++ src/SubArray.java
@@ -17,14 +17,55 @@ public class SubArray {
         }
     }
 
-    // Maximum subarray sum
+    // Maximum subarray sum BRUTEFORCE APPROACH
 
+//    public static void maxSubArraySum(int arr[]){
+//        int currSum = 0;
+//        int maxSum = Integer.MIN_VALUE;
+//
+//        for (int i = 0; i < arr.length; i++){
+//            for (int j = i; j < arr.length; j++){
+//                currSum = 0;
+//                for (int k = i; k <= j; k++){
+//                    currSum += arr[k];
+//                }
+//                System.out.println(currSum);
+//                if (maxSum < currSum){
+//                    maxSum = currSum;
+//                }
+//            }
+//        }
+//        System.out.println("Max Sub Array Sum = " + maxSum);
+//    }
+
+//    Maximum subarray sum PREFIX APPROACH
     public static void maxSubArraySum(int arr[]){
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[arr.length];
 
+        prefix[0] = arr[0];
+        // calculate prefix sum
+        for (int i = 1; i < arr.length; i++) {
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                currSum = i == 0 ? prefix[j] :  prefix[j] - prefix[i-1];
+
+                if (maxSum < currSum){
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("Max Sub Array Sum = " + maxSum);
     }
+
 
     public static void main(String[] args) {
         int arr[] = {2, 4, 6, 8, 10};
         subArray(arr);
+        maxSubArraySum(arr);
     }
 }
