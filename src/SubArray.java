d8a2ec2061f83c0123d5157a9659a186eb6e94df
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-05-08 10:33:47 +0530

    array1

--- src/SubArray.java
+++ src/SubArray.java
@@ -1,21 +1,25 @@
 public class SubArray {
 
-    // Print Subarray and also print sum of sub array
-    public static void subArray(int[] arr){
+//---------------------------------------------------------------------------------------------------------------------
 
-        for (int i = 0; i < arr.length; i++){
-            for (int j = i; j < arr.length; j++ ) {
-                int sum = 0;
-                for (int k = i; k <= j; k++) {
-                    System.out.print(arr[k] + " ");
-                    sum = sum + arr[k];
-                }
-                System.out.print(" = " + sum);
-                System.out.println();
-            }
-            System.out.println();
-        }
-    }
+//    // Print Subarray and also print sum of sub array
+//    public static void subArray(int[] arr){
+//
+//        for (int i = 0; i < arr.length; i++){
+//            for (int j = i; j < arr.length; j++ ) {
+//                int sum = 0;
+//                for (int k = i; k <= j; k++) {
+//                    System.out.print(arr[k] + " ");
+//                    sum = sum + arr[k];
+//                }
+//                System.out.print(" = " + sum);
+//                System.out.println();
+//            }
+//            System.out.println();
+//        }
+//    }
+
+//---------------------------------------------------------------------------------------------------------------------
 
     // Maximum subarray sum BRUTEFORCE APPROACH
 
@@ -37,6 +41,8 @@ public class SubArray {
 //        }
 //        System.out.println("Max Sub Array Sum = " + maxSum);
 //    }
+
+//    ------------------------------------------------------------------------------------------------------------------
 
 //    Maximum subarray sum PREFIX APPROACH
 //    public static void maxSubArraySum(int[] arr){
@@ -61,33 +67,27 @@ public class SubArray {
 //        System.out.println("Max Sub Array Sum = " + maxSum);
 //    }
 
+//    -----------------------------------------------------------------------------------------------------------------
+
 //    Maximum subarray sum KADANES ALGORITHM
     public static void maxSubArraySum(int[] arr){
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
-        int[] prefix = new int[arr.length];
 
-        prefix[0] = arr[0];
-        // calculate prefix sum
-        for (int i = 1; i < arr.length; i++) {
-            prefix[i] = prefix[i-1] + arr[i];
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < arr.length; j++) {
-                currSum = i == 0 ? prefix[j] :  prefix[j] - prefix[i-1];
-                if (maxSum < currSum){
-                    maxSum = currSum;
-                }
+        for (int i = 0; i < arr.length; i++){
+            currSum = currSum + arr[i];
+            if (currSum < 0){
+                currSum = 0;
             }
+            maxSum = Math.max(currSum, maxSum);
         }
-        System.out.println("Max Sub Array Sum = " + maxSum);
+        System.out.println("Max Sub Array Sum is : " + maxSum);
     }
 
 
     public static void main(String[] args) {
-        int[] arr = {1, -2, 6, -1, 3};
-        subArray(arr);
+        int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+//        subArray(arr);
         maxSubArraySum(arr);
     }
 }
