394ee0f1477bc84f96bbc88d05fbe2c833db5a2d
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-05-08 10:12:10 +0530

    array1

--- src/SubArray.java
+++ src/SubArray.java
@@ -1,7 +1,7 @@
 public class SubArray {
 
     // Print Subarray and also print sum of sub array
-    public static void subArray(int arr []){
+    public static void subArray(int[] arr){
 
         for (int i = 0; i < arr.length; i++){
             for (int j = i; j < arr.length; j++ ) {
@@ -39,10 +39,33 @@ public class SubArray {
 //    }
 
 //    Maximum subarray sum PREFIX APPROACH
-    public static void maxSubArraySum(int arr[]){
+//    public static void maxSubArraySum(int[] arr){
+//        int currSum = 0;
+//        int maxSum = Integer.MIN_VALUE;
+//        int[] prefix = new int[arr.length];
+//
+//        prefix[0] = arr[0];
+//        // calculate prefix sum
+//        for (int i = 1; i < arr.length; i++) {
+//            prefix[i] = prefix[i-1] + arr[i];
+//        }
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = i; j < arr.length; j++) {
+//                currSum = i == 0 ? prefix[j] :  prefix[j] - prefix[i-1];
+//                if (maxSum < currSum){
+//                    maxSum = currSum;
+//                }
+//            }
+//        }
+//        System.out.println("Max Sub Array Sum = " + maxSum);
+//    }
+
+//    Maximum subarray sum KADANES ALGORITHM
+    public static void maxSubArraySum(int[] arr){
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
-        int prefix[] = new int[arr.length];
+        int[] prefix = new int[arr.length];
 
         prefix[0] = arr[0];
         // calculate prefix sum
@@ -53,7 +76,6 @@ public class SubArray {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
                 currSum = i == 0 ? prefix[j] :  prefix[j] - prefix[i-1];
-
                 if (maxSum < currSum){
                     maxSum = currSum;
                 }
@@ -64,7 +86,7 @@ public class SubArray {
 
 
     public static void main(String[] args) {
-        int arr[] = {2, 4, 6, 8, 10};
+        int[] arr = {1, -2, 6, -1, 3};
         subArray(arr);
         maxSubArraySum(arr);
     }
