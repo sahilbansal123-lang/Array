23d5b38145ae59cc8fcb6035453e703123c7c765
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-05-21 12:02:49 +0530

    array1

---
+++ src/array2d/SearchinInSortedMatrix.java
@@ -0,0 +1,12 @@
+package array2d;
+
+public class SearchinInSortedMatrix {
+
+    public static void searching( int matrix [][] ){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
