6e6b1912810ca4170a47a5b1aa553bba86b355f3
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-05-21 12:27:39 +0530

    array1

--- src/array2d/SearchinInSortedMatrix.java
+++ src/array2d/SearchinInSortedMatrix.java
@@ -2,11 +2,33 @@ package array2d;
 
 public class SearchinInSortedMatrix {
 
-    public static void searching( int matrix [][] ){
 
+//  In this we use STAIRCASE SEARCH
+    public static boolean searching( int matrix [][] , int key ){
+        int row = 0;
+        int col = matrix[0].length-1;
+
+        while (row < matrix.length && col >= 0){
+            if ( matrix[row][col] == key ){
+                System.out.println("key found at index: (" + row +","+ col + ")");
+                return true;
+            } else if (key < matrix[row][col]) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        System.out.println("KEY NOT FOUND");
+        return false;
     }
 
     public static void main(String[] args) {
+        int matrix[][] = {{10, 20, 30, 40},
+                          {15, 25, 35, 45},
+                          {27, 29, 37, 48},
+                          {32, 33, 39, 50}};
+        int key = 33;
+        searching(matrix, key);
 
     }
 }
